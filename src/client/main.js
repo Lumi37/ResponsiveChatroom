0cc186636e9cba5503d8745985a687e211b4b599
kostas <eoppep@eoppep.gr>
2022-12-19 15:11:29 +0200

    att

--- src/client/main.js
+++ src/client/main.js
@@ -1,4 +1,9 @@
+const webSocket = new WebSocket('ws://localhost:443/');
 const userName = document.querySelector('#name')
+const chat = document.querySelector("#chat")
+webSocket.addEventListener("message", (e)=>{
+    chat.value = e.data;
+})
 document.querySelector('#saveName').addEventListener("click",e=>{
     userName.value = 
 })
--- src/server/server.js
+++ src/server/server.js
@@ -3,5 +3,10 @@ import { WebSocketServer } from 'ws'
 
 const server = express()
 const wsServer = new WebSocketServer({port:3001})
-server.use(express.static('src/client/'))
+server.use(express.static('../client/'))
+wsServer.on("connection", (ws)=>{
+    console.log("New client Connected!!!!!!")
+    ws.send()
+    ws.on('close', ()=>console("client Disconnected!"))
+})
 server.listen(3000, ()=>console.log("listens to 3000") )
