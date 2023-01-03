b521ae98580a65048ab1f37e6a4835333543531f
kostas <eoppep@eoppep.gr>
2023-01-03 15:47:29 +0200

    svdeptrs

--- src/client/main.js
+++ src/client/main.js
@@ -7,12 +7,15 @@ const chat = document.querySelector("#chat")
 const sendButton = document.querySelector("#sendMessage")
 const saveNameBtn = document.querySelector("#saveName")
 let clientID = localStorage.getItem('ClientID')
+const list = document.querySelector('#list')
+let listArr = []
 
     //SENDING INFO FROM LOCALSTORAGE
-webSocket.addEventListener("open",()=>{
+webSocket.addEventListener("open",async ()=>{
     console.log("Client connected Succesfuly")
-    sendInfoFromLocalStorage()
-    refreshList()
+    await sendInfoFromLocalStorage()
+    webSocket.send(JSON.stringify({type:'list'}))
+   // refreshList()
 })
     //SAVING NAME, SENDING TO SERVER
 saveNameBtn.addEventListener('click',e=>{
@@ -33,18 +36,17 @@ webSocket.addEventListener("message", (e)=>{
     console.log("received message",e.data)
     let data = JSON.parse(e.data)
     let typeofMessage = MessageType(data.type)
-    
+    console.log('typeofMessage = ',data.type)
     if(typeofMessage == 'message')
         handleIfMessage(data) 
-    // if(textfieldMessage == 'list')
-    //     handleIfList()
+    if(typeofMessage == 'list')
+        handleIfList(data)
 
 })
 
 document.querySelector('#clear').addEventListener("click",(e)=>{
     chat.innerHTML = ''
 })
-
 
 
 
@@ -64,22 +66,27 @@ function MessageType(type){
     }
 }
 
+
 function handleIfMessage(message){
-if (message.name == textfieldName.value)
+if (message.name == textfieldName.value && message.userID == localStorage.getItem('ClientID'))
     chat.innerHTML += `<p class="user">${message.date} ${message.name}: ${message.text}</p><br>`
 else
     chat.innerHTML += `<p class="otheruser">${message.date} ${message.name}: ${message.text}</p><br>`
 }
 
+
     //GENERATES RANDOM ID
 function clientUniqueIDGenerator(){
+    let datenow = new Date()
     function idGen(){
+        
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     }
-    return idGen() + idGen() + '-' + idGen()
+    return idGen() + idGen() + '-' + idGen() + String(datenow.getMilliseconds())
 }
 
-function sendInfoFromLocalStorage(){
+
+ async function sendInfoFromLocalStorage(){
     if(!(localStorage.getItem('ClientID')))
        window.localStorage.setItem('ClientID',clientUniqueIDGenerator()) //REGISTER ID TO STORAGE
     else{
@@ -92,23 +99,46 @@ function sendInfoFromLocalStorage(){
     } 
 }
 
+
 function sendMessageToServer(){
     let ChatText = textfieldMessage.value;
     let userName = textfieldName.value;
+    let clientID = localStorage.getItem('ClientID')
     textfieldMessage.value = ''
-    let messageToServer = { text:ChatText, name:userName, type:'message'}
-    webSocket.send(JSON.stringify(messageToServer))
+    let messageToServer = { text:ChatText, name:userName, type:'message',uniqueID:clientID}
+    if(ChatText!='')
+        webSocket.send(JSON.stringify(messageToServer))
 }
+
+
 function sendSaveName(){
     let user = textfieldName.value 
-    let clientID = localStorage.getItem('ClientID')
-    let messageToServer = { text:user, type:'name', uniqueID:clientID}
     window.localStorage.setItem('name',user) //REGISTER NAME TO STORAGE
     window.localStorage.setItem('ClientID',clientUniqueIDGenerator())
+    let clientID = localStorage.getItem('ClientID')
+    let messageToServer = { text:user, type:'name', uniqueID:clientID}
     webSocket.send(JSON.stringify(messageToServer))
     document.querySelector("#hello").textContent = 'Hello ' + textfieldName.value   
 }
-function handleIfList
-function refreshList(){
+
+
+function handleIfList(data){
+    if ( listArr.includes(data.name) && listArr.includes(data.id) )
+        listArr.pop()
+    else
+        listArr.push(data)
+    listArr.forEach(elem => {
+        let li = document.createElement('li')
+        li.appendChild(document.createTextNode(elem.name))
+        list.appendChild(li)
+    });
     
+    
+    
+    //  list.innerHTML += `<li>${data.name}<li>`
+
+
 }
+//  function refreshList(){
+    
+//  }
