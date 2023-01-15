256151cb0bb9e945ed2dc0e2e55e8429369b067e
Lumi <37skmg@gmail.com>
2023-01-15 13:40:25 +0200

    fileupload

--- src/client/main.js
+++ src/client/main.js
@@ -12,59 +12,59 @@ const storageID = localStorage.getItem('ClientID')
 let noEdit = true
 // const storageName = localStorage.getItem('name')
 
-    //SENDING INFO FROM LOCALSTORAGE
-webSocket.addEventListener("open", ()=>{
+//SENDING INFO FROM LOCALSTORAGE
+webSocket.addEventListener("open", () => {
     console.log("Client connected Succesfuly")
     sendInfoFromLocalStorage()
-   // refreshList()
+    // refreshList()
 })
-    
-    //SAVING NAME, SENDING TO SERVER
-saveNameButton.addEventListener('click',e=>{console.log("Save name button pressed!"); sendSaveName() })
-    
-    //EDIT NAME
-editNameButton.addEventListener('click',e=>{ console.log("Edit name button pressed!"); nameEdit() })
 
-    //SENDING MESSAGE TO SERVER
-sendButton.addEventListener("click", e=>{ console.log("Send Message button pressed!"); sendMessageToServer() })
+//SAVING NAME, SENDING TO SERVER
+saveNameButton.addEventListener('click', e => { console.log("Save name button pressed!"); sendSaveName() })
 
-textfieldMessage.addEventListener('keypress',e=>{
-    const key= e.key
-    if(key == 'Enter'){
+//EDIT NAME
+editNameButton.addEventListener('click', e => { console.log("Edit name button pressed!"); nameEdit() })
+
+//SENDING MESSAGE TO SERVER
+sendButton.addEventListener("click", e => { console.log("Send Message button pressed!"); sendMessageToServer() })
+
+textfieldMessage.addEventListener('keypress', e => {
+    const key = e.key
+    if (key == 'Enter') {
         sendMessageToServer()
-    }       
+    }
 })
 
 
-    //REFRESH LIST REQ
-refreshBtn.addEventListener('click', e=>{console.log("Refresh List button pressed!"); refreshList() })
+//REFRESH LIST REQ
+refreshBtn.addEventListener('click', e => { console.log("Refresh List button pressed!"); refreshList() })
 
 
-    //RECEIVING MESSAGE FROM SERVER
-webSocket.addEventListener("message", (e)=>{
-    console.log("received message",e.data)
+//RECEIVING MESSAGE FROM SERVER
+webSocket.addEventListener("message", (e) => {
+    console.log("received message", e.data)
     let data = JSON.parse(e.data)
     let typeofMessage = MessageType(data.type)
-    console.log('typeofMessage = ',data.type)
-    if(typeofMessage == 'message')
-        handleIfMessage(data) 
-    if(typeofMessage == 'list')
+    console.log('typeofMessage = ', data.type)
+    if (typeofMessage == 'message')
+        handleIfMessage(data)
+    if (typeofMessage == 'list')
         handleIfList(data)
-    if(typeofMessage == 'history')
+    if (typeofMessage == 'history')
         handleIfHistory(data)
 
 })
 
-    //CLEAR CHAT
-document.querySelector('#clear').addEventListener("click",(e)=>{
+//CLEAR CHAT
+document.querySelector('#clear').addEventListener("click", (e) => {
     chat.innerHTML = ''
 })
 
 
-    //FUNCTIONS 
+//FUNCTIONS 
 
-function MessageType(type){
-    switch (type){
+function MessageType(type) {
+    switch (type) {
         case 'message':
             return 'message'
         case 'list':
@@ -77,117 +77,119 @@ function MessageType(type){
 }
 
 
-function handleIfMessage(messageInfo){
-if (messageInfo.name == textfieldName.value && messageInfo.id == localStorage.getItem('ClientID')){
-    let message = (`<p class="user">${messageInfo.date} ${messageInfo.name}: ${messageInfo.text}</p><br>`)
-    chat.innerHTML += message
+function handleIfMessage(messageInfo) {
+    if (messageInfo.name == textfieldName.value && messageInfo.id == localStorage.getItem('ClientID')) {
+        let message = (`<p class="user">${messageInfo.date} ${messageInfo.name}: ${messageInfo.text}</p><br>`)
+        chat.innerHTML += message
 
-} 
-else{
-    let message =   `<p class="otheruser">${messageInfo.date} ${messageInfo.name}: ${messageInfo.text}</p><br>`
-    chat.innerHTML += message
+    }
+    else {
+        let message = `<p class="otheruser">${messageInfo.date} ${messageInfo.name}: ${messageInfo.text}</p><br>`
+        chat.innerHTML += message
+    }
+
 }
-    
-}
 
 
 
 
-function sendMessageToServer(){
+function sendMessageToServer() {
     let ChatText = textfieldMessage.value;
     let userName = textfieldName.value;
     let storeID = localStorage.getItem('ClientID')
     textfieldMessage.value = ''
-    let messageToServer = { text:ChatText, name:userName, type:'message', id:storeID }
-    if(ChatText!='')
+    let messageToServer = { text: ChatText, name: userName, type: 'message', id: storeID }
+    if (ChatText != '')
         webSocket.send(JSON.stringify(messageToServer))
 }
 
 
-function sendSaveName(){
-    if(!textfieldName.value==''){
+function sendSaveName() {
+    if (!textfieldName.value == '') {
         saveNameButton.disabled = true
+        textfieldMessage.disabled = false
+        sendButton.disabled = false
         editname.disabled = false
-        window.localStorage.setItem('name',textfieldName.value)
-        if(noEdit)
-            window.localStorage.setItem('ClientID',clientUniqueIDGenerator())
+        window.localStorage.setItem('name', textfieldName.value)
+        if (noEdit)
+            window.localStorage.setItem('ClientID', clientUniqueIDGenerator())
         let storeID = localStorage.getItem('ClientID')
         let name = textfieldName.value
-        let messageToServer = { text:name , type:'name', id:storeID }
+        let messageToServer = { text: name, type: 'name', id: storeID }
         webSocket.send(JSON.stringify(messageToServer))
-        document.querySelector("#hello").textContent = 'Hello ' + textfieldName.value   
+        document.querySelector("#hello").textContent = 'Hello ' + textfieldName.value
     }
 }
 
-function nameEdit(){
-    saveNameButton.disabled = false 
+function nameEdit() {
+    saveNameButton.disabled = false
     editname.disabled = false
     noEdit == false
 
 }
 
 
-function handleIfList(listItem){
+function handleIfList(listItem) {
 
     list.innerHTML = ''
     let userList = listItem.list
     console.log(userList)
     userList.forEach(elem => {
-        if(elem.status === 'online')
-            list.innerHTML  += `<li>${elem.name}  <img id="statusicon" src="images/greenicon.png" alt="online"></li>`
-        if(elem.status === 'offline')
-            list.innerHTML  += `<li>${elem.name}  <img id="statusicon" src="images/greyicon.png" alt="offline"></li>`
+        if (elem.status === 'online')
+            list.innerHTML += `<li>${elem.name}  <img id="statusicon" src="images/greenicon.png" alt="online"></li>`
+        if (elem.status === 'offline')
+            list.innerHTML += `<li>${elem.name}  <img id="statusicon" src="images/greyicon.png" alt="offline"></li>`
     })
 }
 
 
-function handleIfHistory(messageInfo){
-    if (messageInfo.name == textfieldName.value && messageInfo.id == localStorage.getItem('ClientID')){
+function handleIfHistory(messageInfo) {
+    if (messageInfo.name == textfieldName.value && messageInfo.id == localStorage.getItem('ClientID')) {
         let message = (`<p class="user">${messageInfo.date} ${messageInfo.name}: ${messageInfo.text}</p><br>`)
         chat.innerHTML += message
-    
-    } 
-    else{
-        let message =   `<p class="otheruser">${messageInfo.date} ${messageInfo.name}: ${messageInfo.text}</p><br>`
+
+    }
+    else {
+        let message = `<p class="otheruser">${messageInfo.date} ${messageInfo.name}: ${messageInfo.text}</p><br>`
         chat.innerHTML += message
     }
 
 }
 
 
-function refreshList(){
+function refreshList() {
     list.innerHTML = ''
-    webSocket.send(JSON.stringify({type:'list'}))
+    webSocket.send(JSON.stringify({ type: 'list' }))
 }
 
 
-    //GENERATES RANDOM ID
-function clientUniqueIDGenerator(){
+//GENERATES RANDOM ID
+function clientUniqueIDGenerator() {
     let datenow = new Date()
-    function idGen(){
-        
+    function idGen() {
+
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     }
     return idGen() + idGen() + '-' + idGen() + String(datenow.getMilliseconds())
 }
 
 
-    //SENDING USER INFO FROM LOCALSTORAGE
-function sendInfoFromLocalStorage(){
-    if(!(localStorage.getItem('ClientID')))
-       window.localStorage.setItem('ClientID',clientUniqueIDGenerator()) //REGISTER ID TO STORAGE
-    else{
-        saveNameButton.disabled = true 
+//SENDING USER INFO FROM LOCALSTORAGE
+function sendInfoFromLocalStorage() {
+    if (!(localStorage.getItem('ClientID')))
+        window.localStorage.setItem('ClientID', clientUniqueIDGenerator()) //REGISTER ID TO STORAGE
+    else {
+        saveNameButton.disabled = true
         editNameButton.disabled = false
         sendButton.disabled = false
         textfieldMessage.disabled = false
         const user = localStorage.getItem('name')
         let clientID = localStorage.getItem('ClientID');
-        let messageToServer = {text:user, type:'name',id:clientID}
+        let messageToServer = { text: user, type: 'name', id: clientID }
         document.querySelector("#hello").textContent = 'Hello ' + user
-        textfieldName.value =  user
+        textfieldName.value = user
         webSocket.send(JSON.stringify(messageToServer))
-        webSocket.send(JSON.stringify({type:'history'}))
-    } 
+        webSocket.send(JSON.stringify({ type: 'history' }))
+    }
 }
 
--- src/server/server.mjs
+++ src/server/server.mjs
@@ -5,99 +5,89 @@ import { WebSocketServer } from 'ws'
 // import _ from 'lodash'
 
 const server = express()
-const wsServer = new WebSocketServer({port:3001})
-server.use(express.static('/home/kostas/projects/test6/ResponsiveChatroom/src/client/'))
+const wsServer = new WebSocketServer({ port: 3001 })
+server.use(express.static('/home/lumi/ResponsiveChatroom/src/client/'))
 server.use(express.json());
-server.use(fileUpload());
+server.use(fileUpload({
+    limits: {
+        fileSize: 20000000 //20 MB
+    }
+}));
+
 let ConnectedClients = 0
 let users = [];
 let list = [];
 let history = []
 
-server.post('/', function(req, res) {
-    let sampleFile;
-    let uploadPath;
-  
-    if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send('No files were uploaded.');
-    }
-
-    sampleFile = req.files.sampleFile;
-    uploadPath ='/home/kostas/projects/test6/ResponsiveChatroom/images/profilepictures/' + sampleFile.name;
-
-    sampleFile.mv(uploadPath, function(err) {
-      if (err)
-        return res.status(500).send(err);
-  
-      console.log('Received a file!')
-    });
-  });
+server.post('/', function (req, res) {
+    handleFile(req, res)
+});
 
 
-wsServer.on("connection", (ws)=>{
-    
+wsServer.on("connection", (ws) => {
+
     ws.id = null
     ws.userName = ''
     ws.status = 'online'
-    
-    users.push(ws) 
-    console.log("New client Connected!")
-    ConnectedClients += 1 
-   
-        // MESSAGES
 
-    ws.on("message",  clientMessage=>{
+    users.push(ws)
+    console.log("New client Connected!")
+    ConnectedClients += 1
+
+    // MESSAGES
+
+    ws.on("message", clientMessage => {
         let data = JSON.parse(clientMessage)
-        console.log("Received: ",data)
+        console.log("Received: ", data)
         const messageType = identifyMessageType(data.type)
-        if(messageType==='name'){
-            if(identifyUserById(data.id)){ setUserInfo(data.id,data.text,ws) }     // ws = JSON.parse(JSON.stringify(identifyUserById(data.id,ws)))//_.cloneDeep(identifyUserById(data.id,ws)) //"DEEPCLONE" EXISTING USER '
-            else{ setNewUserInfo(data.id,data.text,ws) }
+        if (messageType === 'name') {
+            if (identifyUserById(data.id)) { setUserInfo(data.id, data.text, ws) }     // ws = JSON.parse(JSON.stringify(identifyUserById(data.id,ws)))//_.cloneDeep(identifyUserById(data.id,ws)) //"DEEPCLONE" EXISTING USER '
+            else { setNewUserInfo(data.id, data.text, ws) }
         }
-        if(messageType==='message'){
-             handleIfMessage(data.text,data.name,data.id)
+        if (messageType === 'message') {
+            handleIfMessage(data.text, data.name, data.id)
         }
-        if(messageType === 'list'){ handleIfList() }
-        if(messageType === 'history'){ handleIfHistory() }
-        
-        console.log("Current List: ",users.length)
-    })  
+        if (messageType === 'list') { handleIfList() }
+        if (messageType === 'history') { handleIfHistory() }
+
+        console.log("Current List: ", users.length)
+    })
     console.log('\n\n\n-----------------USERLIST-----------------')
-    list.forEach(user=>console.log('--->',user.name,' : ',user.id))
+    list.forEach(user => console.log('--->', user.name, ' : ', user.id))
     console.log(`Connected Clients : ${ConnectedClients}\nList: ${list.length}`)
-    
-        // WHEN CLOSING
-    
-    ws.on('close', ()=>{                                        
+
+    // WHEN CLOSING
+
+    ws.on('close', () => {
         ConnectedClients -= 1
-        try{ 
-            list[list.findIndex(user=> user.id === ws.id)].status = 'offline'
+        try {
+            list[list.findIndex(user => user.id === ws.id)].status = 'offline'
         }
-        catch(error){
+        catch (error) {
             console.log('unregistered client')
         }
         ws.status = 'offline'
-        if(ws.userName != undefined)
+        if (ws.userName != undefined)
             console.log(`${ws.userName} Disconnected! (status:${ws.status})`)
         else
             console.log('A client Disconnected!')
         console.log(`*78*Connected Clients : ${ConnectedClients}\nList: ${users.length}`)
-        
-       // updateUserArray()
+
+        // updateUserArray()
     })
 })
-server.listen(3000, ()=>console.log("listens to 3000") )
+server.listen(3000, () => console.log("listens to 3000"))
 
 
 
 
 
-        //FUNCTIONS
+//FUNCTIONS
 
 
-function identifyMessageType(type){
+function identifyMessageType(type) {
     console.log('\n\n\n-----------------identifyMessageType-----------------')
-    switch(type){
+    switch (type) {
         case 'name':
             console.log('identifyMessageType returns "name"')
             return 'name';
@@ -117,82 +107,100 @@ function identifyMessageType(type){
     }
 }
 
-function setNewUserInfo(uID,username,connection){
+function setNewUserInfo(uID, username, connection) {
     console.log('\n\n\n-----------------setNewUserInfo-----------------')
-    connection.userName = username 
+    connection.userName = username
     connection.id = uID
-    list.push( {name:username, id:uID, status:'online'} )
+    list.push({ name: username, id: uID, status: 'online' })
     console.log(`User ${connection.userName} Connected!`)
 }
 
-function handleIfList(){
+function handleIfList() {
     console.log('\n\n\n-----------------handleIfList-----------------')
-    list.forEach((user,i)=>{
-      console.log(`List:\n${user.name} : ${user.id} : ${user.status}`)
+    list.forEach((user, i) => {
+        console.log(`List:\n${user.name} : ${user.id} : ${user.status}`)
     })
-    users.forEach(user=>{
-        user.send(JSON.stringify({list,type:'list'}))
+    users.forEach(user => {
+        user.send(JSON.stringify({ list, type: 'list' }))
     })
-    
+
 }
-function handleIfHistory(){
-    console.log('\n\n\n-----------------handleIfHistory-----------------\nLoading: ',history.length,'messages...')
-    users.forEach(user =>{
-        history.forEach(messageObject=>{
+function handleIfHistory() {
+    console.log('\n\n\n-----------------handleIfHistory-----------------\nLoading: ', history.length, 'messages...')
+    users.forEach(user => {
+        history.forEach(messageObject => {
             let messageToClient = messageObject
             user.send(messageToClient)
         })
-        
+
     })
 
 }
 
-function  handleIfMessage(message, Name, userID){
+function handleIfMessage(message, Name, userID) {
     console.log('\t\n\n\n-----------------handleIfMessage-----------------')
     const currentdate = dateNow()
-    let messageToClient =JSON.stringify({date:currentdate, name:Name, text:message, type:'message', id:userID})
-    let historyText = JSON.stringify({date:currentdate, name:Name, text:message, type:'history', id:userID})
+    let messageToClient = JSON.stringify({ date: currentdate, name: Name, text: message, type: 'message', id: userID })
+    let historyText = JSON.stringify({ date: currentdate, name: Name, text: message, type: 'history', id: userID })
     history.push(historyText) //SAVING TO HISTORY ARR
-    console.log('Final Message: ',messageToClient)
-    users.forEach(user =>{
-        user.send( (messageToClient))
+    console.log('Final Message: ', messageToClient)
+    users.forEach(user => {
+        user.send((messageToClient))
     })
 
 }
-    // CURRENT DATE
-function dateNow(){
+// CURRENT DATE
+function dateNow() {
     let currentdate = new Date();
-    const datetime =currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds()
+    const datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds()
     return datetime
 }
 
-function identifyUserById(id){
+function identifyUserById(id) {
     let outcome = false
-    console.log('\n\n\n-----------------identifyUserById-----------------\nlist.length: ',list.length)
+    console.log('\n\n\n-----------------identifyUserById-----------------\nlist.length: ', list.length)
     list.forEach(user => {
-        if (id === user.id){
-            console.log('user identified as ',user.name,'with ID:',user.id)
+        if (id === user.id) {
+            console.log('user identified as ', user.name, 'with ID:', user.id)
             console.log('RETURNS TRUE')
             outcome = true
         }
     })
-    if(outcome){ return outcome }
-    else{ console.log('RETURNS FALSE'); return outcome }
+    if (outcome) { return outcome }
+    else { console.log('RETURNS FALSE'); return outcome }
 }
 
-function setUserInfo(id,name,connection){
+function setUserInfo(id, name, connection) {
     console.log('\n\n\n-----------------setUserInfo-----------------')
     list.forEach(user => {
-        if (id === user.id){
+        if (id === user.id) {
             connection.userName = name
             connection.id = id
             user.status = 'online'
-            console.log('user identified as ',connection.userName,'with ID:',connection.id)
+            console.log('user identified as ', connection.userName, 'with ID:', connection.id)
         }
     });
 }
 
+function handleFile(req, res) {
+    let sampleFile;
+    let uploadPath;
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
 
+    sampleFile = req.files.sampleFile;
+    uploadPath = '/home/lumi/ResponsiveChatroom/images/profilepictures/' + sampleFile.name;
+
+    console.log('file size: ', req.files.sampleFile.size)
+    sampleFile.mv(uploadPath, function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        console.log('Received a file!')
+    });
+
+}
 
 // function updateUserArray(id,connection){
 //     console.log('\n\n\n-----------------updateUserArray-----------------')
